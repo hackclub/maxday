b829fbf12be49ea4512794377a77cf6fda949464
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-04-24 22:13:02 -0400

    Switch to LargeButton for Leave a Card

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,40 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
-import { Container, Box, Card, Heading, Text } from '@hackclub/design-system'
+import {
+  Container,
+  Box,
+  Card,
+  Heading,
+  Text,
+  LargeButton
+} from '@hackclub/design-system'
 import Body from '../components/Body'
 import Cards from '../components/Cards'
 
 Card.a = Card.withComponent('article')
 
 export default ({ data: { allMarkdownRemark: { edges } } }) => (
-  <Cards>
-    <Card.a p={4} bg="white" style={{ gridColumn: 'span 2' }}>
-      <Heading.h3 f={5} mb={1} color="primary" align="center" caps>
-        <a href="https://github.com/hackclub/maxday">Leave a Card</a>
-      </Heading.h3>
-    </Card.a>
-    {edges.map(({ node: { excerpt, frontmatter: { author } } }) => (
-      <Card.a p={4} bg="white" key={author}>
-        <Heading.h3 f={5} mb={1} color="primary">
-          <Link to={`/${author}`}>{author}</Link>
-        </Heading.h3>
-        <Body f={1} dangerouslySetInnerHTML={{ __html: excerpt }} />
-      </Card.a>
-    ))}
-  </Cards>
+  <Box.main align="center">
+    <LargeButton
+      inverted
+      href="https://github.com/hackclub/maxday"
+      target="_blank"
+      mb={4}
+    >
+      Leave a Card
+    </LargeButton>
+    <Cards align="left">
+      {edges.map(({ node: { excerpt, frontmatter: { author } } }) => (
+        <Card.a p={4} bg="white" key={author}>
+          <Heading.h3 f={5} mb={1} color="primary">
+            <Link to={`/${author}`}>{author}</Link>
+          </Heading.h3>
+          <Body f={1} dangerouslySetInnerHTML={{ __html: excerpt }} />
+        </Card.a>
+      ))}
+    </Cards>
+  </Box.main>
 )
 
 export const pageQuery = graphql`
