40ce450a464432814aa76606ef09cd1997e20d0f
Zane <contact@zane.sh>
2018-04-25 15:27:08 -0700

    Make Max's link more prominent

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -4,7 +4,11 @@ import { Container, Heading } from '@hackclub/design-system'
 const Bio = () => (
   <Container px={3} pt={[3, 4, 5]} pb={[3, 4]}>
     <Heading.h1 color="white" align="center" f={[4, 5, 6]}>
-      💖 Happy birthday, Max! 🎉
+      💖 Happy birthday,
+      <a href="https://maxwofford.com">
+        {` `}Max
+      </a>
+      ! 🎉
     </Heading.h1>
     <Heading.h2 color="white" align="center" f={[3, 4]} mt={3} regular>
       Max, you pour your heart and soul into Hack Club.
